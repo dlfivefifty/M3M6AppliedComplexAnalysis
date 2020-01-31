f4e1ccd119c66c8ace6657fbecc5a21bcb71dff5
Sheehan Olver <solver@mac.com>
2020-01-31 16:43:58 +0000

    work on lecture notes

--- src/M3M6AppliedComplexAnalysis.jl
+++ src/M3M6AppliedComplexAnalysis.jl
@@ -1,5 +1,5 @@
 module M3M6AppliedComplexAnalysis
-using Weave, Plots, ComplexPhasePortrait
+using Weave
 
 weave("src/Lecture1.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
 weave("src/Lecture2.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
@@ -7,5 +7,7 @@ weave("src/Lecture3.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*
 weave("src/Lecture4.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
 weave("src/Lecture5.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
 weave("src/Lecture6.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
+weave("src/Lecture7.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
+weave("src/Lecture8.jmd", doctype="md2tex", informat="markdown", out_path=pwd()*"/output/", template="src/template.tpl")
 
 end # module
